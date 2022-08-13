7aed05dc02fcc5fbc38eabd161a2aa70a60c6c57
本当迷 <1474051104@qq.com>
2022-08-13 12:03:45 +0800

    使用spring cache缓存套餐

--- src/main/java/com/bdm/reggie/controller/SetmealController.java
+++ src/main/java/com/bdm/reggie/controller/SetmealController.java
@@ -13,6 +13,8 @@ import com.bdm.reggie.service.SetmealService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -43,6 +45,7 @@ public class SetmealController {
      * @return
      */
     @PostMapping
+    @CacheEvict(value = "setmealCache", allEntries = true) // 删除setmealCach这个缓存分类下的所有数据
     public R<String>save(@RequestBody SetmealDto setmealDto){
         log.info("套餐信息：{}", setmealDto);
         setmealService.saveWithDish(setmealDto);
@@ -97,6 +100,7 @@ public class SetmealController {
      * @return
      */
     @DeleteMapping
+    @CacheEvict(value = "setmealCache", allEntries = true) // 删除setmealCach这个缓存分类下的所有数据
     public R<String> deleteBatch(@RequestParam List<Long> ids){
         log.info("ids: {} ",ids.toString());
 
@@ -106,7 +110,7 @@ public class SetmealController {
     }
 
     /**
-     *
+     * 修改套餐状态
      * @param ids
      * @return
      */
@@ -131,6 +135,7 @@ public class SetmealController {
      * @return
      */
     @GetMapping("/list")
+    @Cacheable(value = "setmealCache", key = "#setmeal.categoryId + '_' + #setmeal.status")
     public R<List<Setmeal>>list(Setmeal setmeal){
         final LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper<>();
         queryWrapper.eq(setmeal.getCategoryId() != null, Setmeal::getCategoryId, setmeal.getCategoryId());
