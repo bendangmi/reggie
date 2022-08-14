bc3b5bb2c5000fb06923ac4af191024f089686b8
本当迷 <1474051104@qq.com>
2022-08-14 21:45:27 +0800

    -knife4j 是为Java MVC框架集成swagger生成Api文档的增强解决方案

--- src/main/java/com/bdm/reggie/filter/LoginCheckFilter.java
+++ src/main/java/com/bdm/reggie/filter/LoginCheckFilter.java
@@ -37,7 +37,11 @@ public class LoginCheckFilter implements Filter {
                 "/backend/**",
                 "/front/**",
                 "/user/sendMsg", // 移动端发送短信
-                "/user/login" // 移动端登录
+                "/user/login", // 移动端登录
+                "/doc.html",
+                "/webjars/**",
+                "/swagger-resources",
+                "/v2/api-docs"
         };
 
         // 2.判断本次请求是否需要处理
